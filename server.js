b1809990e3f49fb70b1df6b14c9b3d3dde1c91d5
Hung Phan <pdhung.info@gmail.com>
2022-06-01 06:37:31 +0700

    Free cors

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const corsOptions = {
     }
   },
 };
-app.use(cors(corsOptions));
+app.use(cors());
 app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.use(bodyParser.json());
